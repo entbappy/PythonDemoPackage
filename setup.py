2eff0c142ef8aeeaa01bf4468264065d22052e8b
Bappy Ahmed <entbappy73@gmail.com>
2021-09-30 00:58:16 +0600

    readme updated

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+import setuptools, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -9,6 +9,7 @@ USER_EMAIL = "entbappy73@gmail.com"
 
 setuptools.setup(
     name=f"{PROJECT_NAME}",
+    packages = find_packages(),
     version="0.0.1",
     author="Bappy Ahmed",
     author_email=USER_EMAIL,
@@ -24,11 +25,12 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    # package_dir={"": "src"},
+    # packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
 
     install_requires=[
-        "pandas"
+        "pandas",
+        "yaml"
     ]
 )
